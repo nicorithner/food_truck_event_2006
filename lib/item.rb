99e90803c0fc6486b0c32226c0cc1b315ccab868
Nico Rithner <nicorithner@gmail.com>
2020-08-05 09:11:43 -0600

    Add test it exists and test it has attributes

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,8 @@
-class MyClass
-	attr_reader :attribute
-	def initialize(attribute)
-		@something = attribute
+class Item
+	attr_reader :name, :price
+	def initialize(data)
+		@name = data[:name]
+		@price = data[:price].gsub('$', '').to_f
 	end
 	
 	def method
--- test/item_test.rb
+++ test/item_test.rb
@@ -1,16 +1,18 @@
 require "./test/test_helper"
 
-class MyClassTest < Minitest::Test 
+class ItemTest < Minitest::Test 
 
   def test_it_exists
-    my_class = MyClass.new
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     
-    assert_instance_of MyClass, my_class
+    assert_instance_of Item, item1
   end
   
   def test_it_has_attributes
-    my_class = MyClass.new
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
 
-    assert_equal ["something"], my_class.some_method
+    assert_equal "Apple Pie (Slice)", item2.name
+    assert_equal 2.50, item2.price
   end
 end
